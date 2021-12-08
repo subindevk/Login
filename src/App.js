97933ccbd1319db75b784705f754413a1b77c130
subindevk <subindevk@debugsbunny.com>
2021-12-08 16:42:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
+import ListUserComponent from './components/ListUserComponent';
+import AddUserComponent from './components/AddUserComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <HeaderComponent />
+        <div className= "container">
+          <Switch>
+              <Route exact path = "/" component = {ListUserComponent}></Route>
+              <Route path = "/register" component = {ListUserComponent}></Route>
+              <Route path = "/add-user" component = {AddUserComponent} ></Route>
+              <Route path = "/edit-employee/:id" component = {AddUserComponent}></Route>
+            </Switch>
+        </div>
+        <FooterComponent />
+        </Router>
     </div>
   );
 }
